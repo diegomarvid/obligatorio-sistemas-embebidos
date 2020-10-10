20a943cd943579db174dd35b8744b65747fbf9a0
Diego Marvid <diegomarvid99@gmail.com>
2020-10-10 20:47:48 -0300

    try https 3

--- app.js
+++ app.js
@@ -42,11 +42,14 @@ const CONFIG = {
 const app = express();
 
 app.use(function(req,res,next) {
-    if (!/https/.test(req.protocol)){
-       res.redirect("https://" + req.headers.host + req.url);
-    } else {
-       return next();
-    } 
+    //Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
+    if (req.get('X-Forwarded-Proto')=='https' || req.hostname == 'localhost') {
+        //Serve Angular App by passing control to the next middleware
+        next();
+    } else if(req.get('X-Forwarded-Proto')!='https' && req.get('X-Forwarded-Port')!='443'){
+        //Redirect if not HTTP with original request URL
+        res.redirect('https://' + req.hostname + req.url);
+    }
   });
 
 app.use(Fingerprint( { parameters:[
