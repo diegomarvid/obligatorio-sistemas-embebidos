48f0c15fa3ea2bf73bc295b7d96fb9692285b51f
Diego Marvid <diegomarvid99@gmail.com>
2020-10-13 20:29:24 -0300

    links y botones con opciones de sensor
    se intentara en un futuro implementar url query para cargar la configuracion dependiendo del sensor

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const app = express();
 app.use(function(req,res,next) {
 
     //Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
-    if (req.get('X-Forwarded-Proto')=='https' || req.hostname == 'localhost' ||  req.hostname == '192.168.0.111' ) {
+    if (req.get('X-Forwarded-Proto')=='https' || req.hostname == 'localhost' ||  req.hostname == '192.168.0.102' ) {
         //Serve Angular App by passing control to the next middleware
         next();
     } else if(req.get('X-Forwarded-Proto')!='https' && req.get('X-Forwarded-Port')!='443'){
